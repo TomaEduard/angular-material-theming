d7e846bd57ff2444925ad33af66d9ef77100b956
EduardToma <eduard.daniel.toma@gmail.com>
2022-06-26 14:02:51 +0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-material-theming';
+
+  typeList: string[] = ['none', 'info', 'warn', 'success'];
+  type: 'success' | 'info' | 'warn' | 'none' = 'success';
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
+  constructor() {
+  }
+
 }
